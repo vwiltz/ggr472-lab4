0e1b99fefdb1ac63425af97d32701dd5baeca363
vwiltz <vanessa.wiltshire@mail.utoronto.ca>
2024-04-22 10:52:25 -0400

    legend WIP

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let maxcollis = 0; // initialize variable to store the maximum count of points
 fetch('https://raw.githubusercontent.com/vwiltz/ggr472-lab4/main/data/pedcyc_collision_06-21.geojson')
     .then(response => response.json())
     .then(response => {
-        myPoints = response; 
+        myPoints = response;
 
         map.on('load', () => {
             map.addSource('collisions', {
@@ -58,19 +58,37 @@ fetch('https://raw.githubusercontent.com/vwiltz/ggr472-lab4/main/data/pedcyc_col
                 'source': 'hexgrid',
                 'paint': {
                     'fill-opacity': 0.5,
-                    'fill-color': 
-                    [
-                        'interpolate',
-                        ['linear'],
-                        ['get', 'COUNT'],
-                        0, '#FFFFCC', // color for zero collisions
-                        maxcollis * 0.25, '#FFCC00',
-                        maxcollis * 0.5, '#FF9900',
-                        maxcollis * 0.75, '#FF6600',
-                        maxcollis, '#800026' // color for max collisions
-                    ],
+                    'fill-color':
+                        [
+                            'interpolate',
+                            ['linear'],
+                            ['get', 'COUNT'],
+                            0, '#FFFFCC', // color for zero collisions
+                            maxcollis * 0.25, '#FFCC00',
+                            maxcollis * 0.5, '#FF9900',
+                            maxcollis * 0.75, '#FF6600',
+                            maxcollis, '#800026' // color for max collisions
+                        ],
                     'fill-outline-color': '#000000'
                 }
             });
         });
+
+        // create legend
+        const legend = document.getElementById('legend');
+
+        layers.forEach((layer, i) => {
+            const color = colors[i];
+            const item = document.createElement('div');
+            const key = document.createElement('span');
+            key.className = 'legend-key';
+            key.style.backgroundColor = color;
+
+            const value = document.createElement('span');
+            value.innerHTML = `${layer}`;
+            item.appendChild(key);
+            item.appendChild(value);
+            legend.appendChild(item);
+        });
+
     });
